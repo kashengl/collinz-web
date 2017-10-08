be1bb08c39e621801751c3e5fae211af7c4f8c5f
Chua Kah Sern <kahsern@alphapod.com>
2017-10-08 18:09:52 +0800

    header top bar

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,27 @@ var reload      = browserSync.reload;
  
 /* Task to compile less */
 gulp.task('compile-less', function() {  
-  gulp.src('./src/main.less')
+  gulp.src('./src/new-main.less')
     .pipe(less())
     .pipe(gulp.dest('./css'));
 }); 
 /* Task to watch less changes */
 gulp.task('watch-less', function() {  
-  gulp.watch('./src/main.less' , ['compile-less']);
+  gulp.watch('./src/new-main.less' , ['compile-less']);
 });
  
-// gulp.task('serve', function () {
+gulp.task('serve', function () {
  
-//     // Serve files from the root of this project
-//     browserSync.init({
-//         server: {
-//             baseDir: "./distro/"
-//         }
-//     }); 
-//     gulp.watch("./src/*.less").on("change", reload);
-//     gulp.watch("./distro/*.html").on("change", reload);
-// });
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    }); 
+    gulp.watch("./src/new-main.less").on("change", reload);
+    gulp.watch("./*.html").on("change", reload);
+});
  
 /* Task when running `gulp` from terminal */
-// gulp.task('default', ['watch-less', 'serve']);
-gulp.task('default', ['watch-less']);
+gulp.task('default', ['watch-less', 'serve']);
+// gulp.task('default', ['watch-less']);
