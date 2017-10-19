45fd1a3139026bd2be401ac8c18f7f22ee5137bb
Chua Kah Sern <kahsern@alphapod.com>
2017-10-20 00:43:21 +0800

    fix css

--- js/main.js
+++ js/main.js
@@ -666,6 +666,19 @@ $(document).ready(function(){
 		console.log('clicked');
 		$('.nav-mobile ').toggleClass('active');
 	});
+
+	//carousel swiping
+	$(".carousel-section").swipe({
+
+	  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+	    if (direction == 'left') $(this).carousel('next');
+	    if (direction == 'right') $(this).carousel('prev');
+
+	  },
+	  allowPageScroll:"vertical"
+
+	});
 });
 
 
