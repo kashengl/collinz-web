120d7469f2aedf7d00391d74f0d4f45920a4ae5e
Chua Kah Sern <kahsern@alphapod.com>
2017-10-29 01:32:15 +0800

    working on carousel

--- js/main.js
+++ js/main.js
@@ -666,19 +666,6 @@ $(document).ready(function(){
 		console.log('clicked');
 		$('.nav-mobile ').toggleClass('active');
 	});
-
-	//carousel swiping
-	$(".carousel-section").swipe({
-
-	  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-	    if (direction == 'left') $(this).carousel('next');
-	    if (direction == 'right') $(this).carousel('prev');
-
-	  },
-	  allowPageScroll:"vertical"
-
-	});
 });
 
 
