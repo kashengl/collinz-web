2ae83d7cd0060c7f61b1a9c1a16a77b59133f224
Chua Kah Sern <kahsern@alphapod.com>
2017-11-05 21:47:26 +0800

    compiled css/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var less = require('gulp-less'); 
+var minifyCss = require('gulp-minify-css');
+var minifyJs = require('gulp-uglify');
+var rename = require('gulp-rename');
+
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
  
@@ -8,13 +12,32 @@ var reload      = browserSync.reload;
 gulp.task('compile-less', function() {  
   gulp.src('./src/new-main.less')
     .pipe(less())
+    .pipe(minifyCss())
+    .pipe(rename({
+        suffix: '.min'
+    }))
     .pipe(gulp.dest('./css'));
 }); 
+
+gulp.task('minify-js', function () {
+    gulp.src('./js/main.js') // path to your files
+    .pipe(minifyJs())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./js'));
+});
+
 /* Task to watch less changes */
 gulp.task('watch-less', function() {  
   gulp.watch('./src/new-main.less' , ['compile-less']);
 });
  
+gulp.task('watch-js', function() {  
+  gulp.watch('./js/main.js' , ['minify-js']);
+});
+ 
+
 gulp.task('serve', function () {
  
     // Serve files from the root of this project
@@ -28,5 +51,15 @@ gulp.task('serve', function () {
 });
  
 /* Task when running `gulp` from terminal */
-gulp.task('default', ['watch-less', 'serve']);
+gulp.task('default', ['watch-less', 'watch-js','serve']);
 // gulp.task('default', ['watch-less']);
+
+//compile old css
+gulp.task('compile-css', function() {  
+  gulp.src('./css/main.css')
+    .pipe(minifyCss())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('./css'));
+}); 
